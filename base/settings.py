fb471fcdb6e517ca9f75dc19ee9498e71af62ea9
Penk13 <natanaelsteven109@gmail.com>
2024-05-15 22:19:06 +0700

    setup staticfiles

--- base/settings.py
+++ base/settings.py
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "static-live" / "static-root"
+MEDIA_ROOT = BASE_DIR / "static-live" / "media-root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
